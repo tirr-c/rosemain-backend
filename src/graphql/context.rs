06356bc9c39df5d92ab739d756bc888396dedc4d
Wonwoo Choi <chwo9843@gmail.com>
2018-12-07 22:29:51 +0900

    간단한 서버 추가

--- src/graphql/context.rs
+++ src/graphql/context.rs
@@ -1,5 +1,16 @@
+use crate::connection::ConnectionPool;
+
+#[derive(Clone)]
 pub struct Context {
-    pub(crate) pool: crate::connection::ConnectionPool,
+    pub(crate) pool: ConnectionPool,
 }
 
 impl juniper::Context for Context {}
+
+impl Context {
+    pub fn new(pool: ConnectionPool) -> Self {
+        Context {
+            pool,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#![feature(async_await, futures_api)]
+
+use http::StatusCode;
+use rosemain::{connection, graphql::{self, Context}};
+use tide::{body, App, AppData, Response};
+
+async fn graphql(
+    ctx: AppData<Context>,
+    query: body::Json<juniper::http::GraphQLRequest>,
+) -> Result<Response, StatusCode> {
+    let response = query.execute(&graphql::new_schema(), &ctx);
+    let body_vec = serde_json::to_vec(&response).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    http::Response::builder()
+        .status(if response.is_ok() {
+            StatusCode::OK
+        } else {
+            StatusCode::BAD_REQUEST
+        })
+        .header("Content-Type", "application/json")
+        .body(body::Body::from(body_vec))
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pool = connection::establish_connection();
+    let ctx = Context::new(pool);
+    let mut app = App::new(ctx);
+
+    app.at("/graphql").post(graphql);
+
+    let address = "127.0.0.1:8000".to_owned();
+    app.serve(address);
 }
