31904d6476a4e9adc665ec9b59e85cd136104b54
Wonwoo Choi <chwo9843@gmail.com>
2019-01-19 17:22:45 +0900

    최신 Tide로 업데이트

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,21 @@
 
 use http::StatusCode;
 use rosemain::{connection, graphql::{self, Context}};
-use tide::{body, App, AppData, Response};
+use tide::{body, configuration, App, AppData, IntoResponse, Response};
 
 async fn graphql(
     ctx: AppData<Context>,
     query: body::Json<juniper::http::GraphQLRequest>,
-) -> Result<Response, StatusCode> {
+) -> Response {
     let response = query.execute(&graphql::new_schema(), &ctx);
-    let body_vec = serde_json::to_vec(&response).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    http::Response::builder()
-        .status(if response.is_ok() {
-            StatusCode::OK
-        } else {
-            StatusCode::BAD_REQUEST
-        })
-        .header("Content-Type", "application/json")
-        .body(body::Body::from(body_vec))
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+    let status = if response.is_ok() {
+        StatusCode::OK
+    } else {
+        StatusCode::BAD_REQUEST
+    };
+    body::Json(response)
+        .with_status(status)
+        .into_response()
 }
 
 fn main() {
@@ -27,8 +24,10 @@ fn main() {
     let ctx = Context::new(pool);
     let mut app = App::new(ctx);
 
+    let conf = configuration::Configuration::build()
+        .port(8000)
+        .finalize();
+    app.config(conf);
     app.at("/graphql").post(graphql);
-
-    let address = "127.0.0.1:8000".to_owned();
-    app.serve(address);
+    app.serve();
 }
